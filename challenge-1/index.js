1d177dcbf821c6d414cfc093fdae2fa35a2da3a4
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-29 09:38:17 +0300

    first commit for challege1 student grade generator

---
+++ challenge-1/index.js
@@ -0,0 +1,22 @@
+function studentMarks (){
+  let grade;
+  if (marks > 79) {
+    grade = 'A'
+  }
+
+  else if (marks => 60 && marks <= 79){
+    grade = 'B'
+  }
+
+ else if (marks < 60 && marks => 49){
+    grade = 'C'
+  }
+
+  else if (marks =>40 && marks < 49){
+    grade = 'D'
+  }
+
+  else{
+    grade = 'E'
+  }
+}
