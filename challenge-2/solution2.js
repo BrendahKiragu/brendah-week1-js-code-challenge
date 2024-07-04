8bda5121471d40bc5b3b414c14ebe04dcc790337
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-04 16:13:45 +0300

    makes ammends on challenge2 files

--- challenge-2/solution2.js
+++ challenge-2/solution2.js
@@ -1,5 +1,12 @@
 //function declaration and variables assignment
 function checkSpeed(speed) {
+    let speed = prompt('Enter speed(>= 0)');
+    while (isNaN(speed) || speed < 0){
+    alert("Invalid input. Please enter a number >=0");
+
+   speed = prompt("Enter speed (>=0)");
+   
+  }
     const speedLimit = 70;
     const kmPerDemeritPoint = 5;
 
