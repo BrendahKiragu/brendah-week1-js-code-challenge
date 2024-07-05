829e06ec33d00625f24a959a1e27fefd914c241c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-05 11:05:46 +0300

    renames files and adds prompt

--- challenge-2/solution2.js
+++ challenge-2/solution2.js
@@ -1,26 +1,30 @@
 //function declaration and variables assignment
-function checkSpeed(speed) {
-    let speed = prompt('Enter speed(>= 0)');
-    while (isNaN(speed) || speed < 0){
-    alert("Invalid input. Please enter a number >=0");
+function checkSpeed(speed) { 
+  const speedLimit = 70;
+    const kmPerDemeritPoint = 5; 
 
-   speed = prompt("Enter speed (>=0)");
-   
-  }
-    const speedLimit = 70;
-    const kmPerDemeritPoint = 5;
-
-    if (speed < speedLimit) {
-        console.log("Ok");
-    } else {
+    if (speed <= speedLimit) {
+        return "Ok";
+    }  else {
         const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemeritPoint);
         if (demeritPoints > 12) {
-            console.log("License suspended");
+           return "License suspended";
         } else {
-            console.log(`Points: ${demeritPoints}`);
+            return`Points: ${demeritPoints}`;
         }
     }
 }
 
-//function call and input an argument inside parenthesis 
-checkSpeed(90);
+
+// Prompts for input(speed) and converts it to a floating-point number
+let userSpeed = parseFloat(prompt("Enter speed"));
+
+if(isNaN(userSpeed) || userSpeed< 0){
+  alert("Please enter a valid number for speed.");  
+
+  userSpeed = parseFloat(prompt("Enter speed"));
+} else{
+    alert(checkSpeed(userSpeed));
+}
+
+
